f5b9f83033a94ce0e93165b492d7513932faa6a5
JuanMaTovar <91153519+JuanMaTovar@users.noreply.github.com>
2023-09-17 10:19:29 -0700

    modulo_10
    Actividad realizada del módulo 10

---
+++ BreakOut/Assets/Scripts/Bloque_Goma.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bloque_Goma : Bloque
+{
+    public int rebote = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        resistencia = 5;
+    }
+
+    // Update is called once per frame
+    //Con override estamos indicando que hacemos la sobrecarga del método RebotarBola()
+    public override void RebotarBola()
+    {
+        base.RebotarBola(); //es el método base de la clase Bloque de la cual estamos heredando
+    }
+}
---
+++ BreakOut/Assets/Scripts/Bloque_Madera.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bloque_Madera : Bloque
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        resistencia = 3;
+    }
+
+    //Con override estamos indicando que hacemos la sobrecarga del método RebotarBola()
+    public override void RebotarBola()
+    {
+        base.RebotarBola(); //es el método base de la clase Bloque de la cual estamos heredando
+    }
+}
